6f42ad59feeea918884b91f1817b431b601aea8b
tuannv6 <tuannv6@vng.com.vn>
2021-02-03 11:08:26 +0700

    edit image name docker

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,7 @@
 # replace "/" by "-", vì docker tag không cho phép dấu "/"
 ORIGINAL_BRAND_NAME=$(git symbolic-ref --short HEAD)
 BRAND_NAME=${ORIGINAL_BRAND_NAME//[\/]/-}
-DOCKER_IMAGE=vision-frontend
+DOCKER_IMAGE=antd-sample
 DOCKER_REGISTRY=vantuan12345
 #-----------------------------------------------------------------------
 docker build . -t ${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${BRAND_NAME}
